83f7d85a82df90393e6873134c19390a9b9c9445
RajKoladiya22 <koladiyaraj22@gmail.com>
2024-04-25 11:29:50 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
     const mongoose = require('mongoose');
 
-    // mongoose.connect("mongodb://localhost:27017/FastKartAPI");
-    mongoose.connect(`mongodb+srv://Raj:uq5rMhO1FfkLkOU3@cluster0.fvudrsz.mongodb.net/API`);
+    mongoose.connect("mongodb://localhost:27017/AWS-API");
+    // mongoose.connect(`mongodb+srv://Raj:uq5rMhO1FfkLkOU3@cluster0.fvudrsz.mongodb.net/API`);
 
 
     const db = mongoose.connection;
